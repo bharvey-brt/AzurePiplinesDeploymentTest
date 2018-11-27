d4d7cde4ec3df372f3aa4c3984778ee8bd58debe
Brandon <brandon.harvey@bigrivertech.com>
2018-11-27 13:16:55 -0500

    Check in test

--- SampleService/SampleService/Controllers/ValuesController.cs
+++ SampleService/SampleService/Controllers/ValuesController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SampleService.Controllers
-                       //wow
+
 {
     [Route("api/[controller]")]
     [ApiController]
